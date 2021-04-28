7f28eadb02416f00da7b12844a6a23e4e5b8ecb4
Shinsuke  May <s.seanmay@gmail.com>
2021-04-28 21:08:21 +0900

    expand functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome('/Users/sean/chromedriver')
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'install' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome('/Users/sean/chromedriver')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retreive_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+if __name__ == '__main__':
+    unittest.main()
 
 
