2bf90ffebe13a0515967a65bdfa6ffb47d5a993f
Shinsuke  May <s.seanmay@gmail.com>
2021-04-29 13:17:54 +0900

    redirect after post, show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -34,7 +34,15 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         self.check_for_row_in_list_table("1: Buy peacock feathers")
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
 
         self.fail('Finish the test!!')
 
