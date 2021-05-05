5ecbfde31ac0926ad4eed9ba91fc6beeb3070b87
Shinsuke  May <s.seanmay@gmail.com>
2021-05-05 13:21:45 +0900

    moced fts into their own individual files

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,9 @@
+from .base import FunctionalTest
+from selenium.webdriver.common.keys import Keys
+from unittest import skip
+
+
+class ItemValidationTest(FunctionalTest):
+
+    def test_cannot_add_empty_list_item(self):
+        self.fail('write me!')
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,32 +1,9 @@
-# from django.test import LiveServerTestCase
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
-import time
 
-MAX_WAIT = 10
 
-class NewVisitorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Chrome('/Users/sean/chromedriver')
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')    
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+class NewVisitorTest(FunctionalTest):
 
     def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
@@ -83,5 +60,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
-
