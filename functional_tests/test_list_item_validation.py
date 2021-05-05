c8615f250d55d01a48a557bdcb6b445ccb0df18c
Shinsuke  May <s.seanmay@gmail.com>
2021-05-05 13:52:00 +0900

    test to check users can't add empty list item

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,4 +6,29 @@ from unittest import skip
 class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_item(self):
+        #Hit submit on empty list item
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empyty list item"
+        ))
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empyty list item"
+        ))
+
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
+
         self.fail('write me!')
